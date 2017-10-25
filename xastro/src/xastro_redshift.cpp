ee1b5cfe4c3be2027dffd721d787ee1c15dec4d6
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2017-10-25 14:49:27 -0500

    xastro: add routine to compute velocity based on observed and rest wavelength

--- xastro/src/xastro_redshift.cpp
+++ xastro/src/xastro_redshift.cpp
@@ -10,3 +10,10 @@ double XA_Observed_Frame_Wavelength(const double & i_dRest_Frame_Wavelength, con
 	return i_dRest_Frame_Wavelength * (1.0 + i_dRedshift);
 }
 
+double	XA_Compute_Velocity(const double & i_dObserved_Wavelength, const double & i_dRest_Wavelength)
+{
+	double	dz = i_dObserved_Wavelength / i_dRest_Wavelength;
+	double	dz_sqr = dz * dz;
+	return (g_XASTRO.k_dc * (dz_sqr - 1.0) / (dz_sqr + 1.0));
+}
+
